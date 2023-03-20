50ef7e97b3ea91e7581a6e91acf9f4af8bc6e428
wxnode <jayden@xnode.cc>
2023-03-21 05:44:50 +0800

    Update user-bspwm.sh

--- user-bspwm.sh
+++ user-bspwm.sh
@@ -1,28 +1,17 @@
 #!/bin/bash
 
-#cd ~
-#git clone https://aur.archlinux.org/yay.git
-#cd ~/yay
-#makepkg -si
-#cd ~
-#rm -rf ~/yay
-sudo dhcpcd
-sleep 15
-yay --aururl "https://aur.tuna.tsinghua.edu.cn" --save
-#安装AMD开源显卡驱动
-yay -S --noconfirm xf86-video-amdgpu mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon
-#安装AMD闭源官方驱动
-#yay -S --noconfirm amdgpu-pro-libgl vulkan-amdgpu-pro
+#安装Intel显卡驱动
+yay -S --noconfirm yay -S --noconfirm mesa
 #安装视频硬解驱动
-yay -S --noconfirm libva-mesa-driver lib32-libva-mesa-driver mesa-vdpau lib32-mesa-vdpau
+yay -S --noconfirm intel-media-driver
 #安装字体
 yay -S --noconfirm noto-fonts noto-fonts-cjk ttf-liberation nerd-fonts-hack nerd-fonts-jetbrains-mono nerd-fonts-droid-sans-mono
-#安装KDE桌面
-yay -S --noconfirm xray v2raya
+#安装Fcitx5输入法
 yay -S --noconfirm fcitx5-im fcitx5-chinese-addons fcitx5-pinyin-zhwiki fcitx5-material-color
 sudo sh -c 'echo "GTK_IM_MODULE=fcitx" >> /etc/environment'
 sudo sh -c 'echo "QT_IM_MODULE=fcitx" >> /etc/environment'
 sudo sh -c 'echo "XMODIFIERS=@im=fcitx" >> /etc/environment'
+#安装oh-my-zsh并设置
 yay -S --noconfirm oh-my-zsh-git
 #sudo git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git /usr/share/oh-my-zsh/custom/themes/powerlevel10k
 cp /usr/share/oh-my-zsh/zshrc ~/.zshrc
@@ -32,19 +21,13 @@ sed -i "70,80s/(git)/(git sudo zsh-autosuggestions zsh-syntax-highlighting z)/g"
 echo "bindkey ',' autosuggest-accept" >> ~/.zshrc
 echo "export EDITOR='nvim'" >> ~/.zshrc
 chsh -s /bin/zsh
-yay -S --noconfirm xdg-user-dirs
-xdg-user-dirs-update
+#安装浏览器
 yay -S --noconfirm firefox
+#安装xorg
 yay -S --noconfirm xorg-server
 yay -S --noconfirm feh picom bspwm polybar alacritty rofi ranger network-manager-applet sxhkd dunst bc pacman-contrib numlockx zenity freerdp
 yay -S --noconfirm arc-gtk-theme lxappearance thunar thunar-archive-plugin xarchiver unrar p7zip tumbler papirus-icon-theme vimix-cursors
-yay -S --noconfirm lightdm lightdm-webkit2-greeter light-locker
+#安装SDDM
+yay -S --noconfirm sddm
 yay -S --noconfirm alsa-utils pulseaudio pulseaudio-alsa pavucontrol
-sudo bash -c "sed -i '100,110s/#greeter-session=example-gtk-gnome/greeter-session=lightdm-webkit2-greeter/g' /etc/lightdm/lightdm.conf"
-cp -r /home/data/dotfiles/bspwm/* ~/.config/
-sudo systemctl enable lightdm NetworkManager v2raya
-sudo localectl set-locale LANG=zh_CN.UTF-8
-echo "export LANGUAGE='en'" >> ~/.zshrc
-echo "alias en='LANGUAGE=en zsh'" >> ~/.zshrc
-echo "alias cn='LANGUAGE=zh_CN zsh'" >> ~/.zshrc
-#sudo bash -c "sed -i '/^webkit_theme/cwebkit_theme        = material2' /etc/lightdm/lightdm-webkit2-greeter.conf"
+sudo systemctl enable sddm NetworkManager
