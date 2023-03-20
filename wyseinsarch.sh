2db9bb952f1f9dd747cd1d7d8803a1e1dda10810
wxnode <jayden@xnode.cc>
2023-03-21 03:42:15 +0800

    Update wyseinsarch.sh

--- wyseinsarch.sh
+++ wyseinsarch.sh
@@ -5,8 +5,8 @@ mkfs.ext4 -F /dev/mmcblk0p2
 mount /dev/mmcblk0p2 /mnt
 mkdir /mnt/boot
 mount /dev/mmcblk0p1 /mnt/boot
-#sed -i "6i Server = https://mirrors.bfsu.edu.cn/archlinux/\$repo/os/\$arch" /etc/pacman.d/mirrorlist
-sed -i "6i Server = https://mirrors.tuna.tsinghua.edu.cn/archlinux/\$repo/os/\$arch" /etc/pacman.d/mirrorlist
+#sed -i "6i Server = https://mirrors.tuna.tsinghua.edu.cn/archlinux/\$repo/os/\$arch" /etc/pacman.d/mirrorlist
+sed -i "6i Server = https://mirrors.bfsu.edu.cn/archlinux/\$repo/os/\$arch" /etc/pacman.d/mirrorlist
 pacstrap /mnt base base-devel linux linux-firmware dhcpcd
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
